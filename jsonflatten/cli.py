65787c81897bbe011536113d4ba49c9ddb9e9972
Timoteo <phillipstr@gmail.com>
2017-10-04 23:51:14 -0400

    Refines the cli interface and handles arrays of json as input.
    Gets rid of the -F (--Flatten) option and instead flattens the entire
    object when no args are set for the -f option.  -f is now allowed multiple
    arguments and can be called like: `-f7,9` or `-f7 -f9`.  In either case
    both keys 7 and 9 are flattened.  Lastly, if the input is an array of JSON,
    then the cli handles each item in the array as a separate JSON document and
    flattens it appropriately for each item in the array.

--- jsonflatten/cli.py
+++ jsonflatten/cli.py
@@ -1,5 +1,8 @@
 """Command-Line Interface."""
 
+from collections import Mapping
+from collections import Sequence
+
 import click
 from click import argument
 from click import option
@@ -8,38 +11,37 @@ from click import version_option
 from jsoncut.cli import load_json
 from jsoncut.cli import output
 from jsoncut.sequencer import Items
-from jsoncut.tokenizer import SLICE_RE, parse_defaults, parse_keystr
+from jsoncut.tokenizer import parse_keystr
 from jsoncut.treecrawler import find_keys
 
 from jsonflatten.core import flatten_by_keys
-from jsonflatten.core import generate_rows
 
 
 def get_results(data, kwds):
-    """ Dispatch to correct function(s) """
-    if kwds['Flatten']:
+    """ Parse args and flatten the document """
+    if kwds['flatten'] == ():
         results = flatten_by_keys(data, keys=None)
-    elif kwds['flatten'] is not None:
+    else:
+        if len(kwds['flatten']) == 1:
+            args = ''.join(kwds['flatten'])
+        else:
+            args = ','.join(kwds['flatten'])
+
         data_ = Items([data] if kwds['slice_'] else data)
         keys = find_keys(data_.value, fullscan=True)
-        keylists = parse_keystr(kwds['flatten'], data.items, quotechar='"',
+        keylists = parse_keystr(args, data.items, quotechar='"',
                                 keys=keys)
-        results = flatten_by_keys(data_.value, keys=['.'.join(key) for
-                                  key in keylists])
+        results = flatten_by_keys(data_.value, keys=['.'.join(key)
+                                  for key in keylists])
     return results
 
 
 @click.command()
 @argument('jsonfile', type=click.Path(readable=True), required=False)
-@option('-F', '--Flatten', 'Flatten', is_flag=True,
-        help='Flatten entire json document.')
-@option('-f', '--flatten', 'flatten',
-        help='Flatten only those specified keys as a comma-separated list ' +
-        'generated from `jsoncut -l` option.')
-@option('-g', '--genrows', 'genrows', is_flag=True,
-        help='If set, generates flattened rows of output if one of the ' +
-        'keys is a Sequence, specified with the -f option, or ' +
-        'automatically if used with the -F option.')
+@option('-f', '--flatten', 'flatten', multiple=True,
+        help=('Flatten only those specified keys generated from `jsoncut -l` '
+              'option as a comma-separated list or idividually, i.e. '
+              ' `-f7,9` or `-f7 -f9`'))
 @option('-n', '--nocolor', is_flag=True, help='disable syntax highlighting')
 @option('-s', '--slice', 'slice_', is_flag=True, help='disable sequencer')
 @version_option(version='0.0', prog_name='JSON Flatten')
@@ -48,8 +50,15 @@ def main(ctx, **kwds):
     """Parse command-line args"""
     ctx.color = False if kwds['nocolor'] else True
     data = load_json(ctx, kwds['jsonfile'])
-    results = get_results(data, kwds)
-    output(ctx, results, indent=4, is_json=True)
+
+    if isinstance(data, Mapping):
+        results = get_results(data, kwds)
+        output(ctx, results, indent=4, is_json=True)
+
+    elif isinstance(data, Sequence):
+        results = [get_results(item, kwds) for item in data]
+        for res in results:
+            output(ctx, res, indent=4, is_json=True)
 
 
 if __name__ == '__main__':
