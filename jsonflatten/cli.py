6f355c4676eb1ddb3f6dc39d1f5940a589338680
Timoteo <phillipstr@gmail.com>
2017-10-05 23:34:01 -0400

    Adds the -q option and small changes to make more pythonic
    Adds the -q (--quotechar) option so the user can set which character
    is used as the quote character in a typical JSON, or similar, data
    format.  Also makes some changes to code to make more readable and
    pythonic.

--- jsonflatten/cli.py
+++ jsonflatten/cli.py
@@ -18,18 +18,15 @@ from jsonflatten.core import flatten_by_keys
 
 
 def get_results(data, kwds):
-    """ Parse args and flatten the document """
-    if kwds['flatten'] == ():
-        results = flatten_by_keys(data, keys=None)
+    """Parse args and flatten the document."""
+    if not kwds['flatten']:
+        return flatten_by_keys(data, keys=None)
     else:
-        if len(kwds['flatten']) == 1:
-            args = ''.join(kwds['flatten'])
-        else:
-            args = ','.join(kwds['flatten'])
+        args = ','.join(kwds['flatten'])
 
         data_ = Items([data] if kwds['slice_'] else data)
         keys = find_keys(data_.value, fullscan=True)
-        keylists = parse_keystr(args, data.items, quotechar='"',
+        keylists = parse_keystr(args, data.items, quotechar=kwds['quotechar'],
                                 keys=keys)
         results = flatten_by_keys(data_.value, keys=['.'.join(key)
                                   for key in keylists])
@@ -42,8 +39,10 @@ def get_results(data, kwds):
         help=('Flatten only those specified keys generated from `jsoncut -l` '
               'option as a comma-separated list or idividually, i.e. '
               ' `-f7,9` or `-f7 -f9`'))
-@option('-n', '--nocolor', is_flag=True, help='disable syntax highlighting')
-@option('-s', '--slice', 'slice_', is_flag=True, help='disable sequencer')
+@option('-n', '--nocolor', is_flag=True, help='Disable syntax highlighting')
+@option('-q', '--quotechar', 'quotechar', default='"',
+        help='Quote character used in serialized data, defaults to \'"\'')
+@option('-s', '--slice', 'slice_', is_flag=True, help='Disable sequencer')
 @version_option(version='0.0', prog_name='JSON Flatten')
 @click.pass_context
 def main(ctx, **kwds):
